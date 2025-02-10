e4ef10a6782a8c74765af4cff41e140158045e55
Hanumaraddi Vaddatti <hanumaraddin14@gmail.com>
2025-02-10 16:05:46 +0530

    integrated api call for getting list of items

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import CardList from './pages/CardList';
 
 function App() {
   return (
-    <div className="App">
-      <CardList/>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-5">Card List</h1>
+      <CardList />
     </div>
   );
 }
--- src/pages/CardList.js
+++ src/pages/CardList.js
@@ -1,9 +1,63 @@
-import React from 'react'
-
+import React, { useEffect, useReducer } from 'react'
+import axios from 'axios'
 const CardList = () => {
-  return (
-    <div>CardList</div>
-  )
+
+    //initial state
+    const initialState = {
+        cardsData: [],
+    }
+
+    //reducer for handle state update
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "commonUpdate":
+                return {
+                    ...state,
+                    ...action.payload,
+                };
+            default:
+                return {
+                    ...state,
+                };
+        }
+    };
+
+
+    //using useReducer hook
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+     //initial function call for getting list of items
+        init()
+        //eslint-disable-next-line
+    }, []);
+
+
+    const init = async () => {
+        let tempState = state;
+        try {
+            //triggering api using axios
+            const result = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10");
+
+            // console.warn("result", result);
+   
+            let { data } = result;
+            //updating the staste with result
+            tempState["cardsData"] = [...data];
+            
+            dispatch({
+                type: "commonUpdate",
+                payload: tempState,
+            });
+        } catch (error) {
+
+            console.error(error)
+        }
+    }
+    console.log("state", state)
+    return (
+        <div>CardList</div>
+    )
 }
 
 export default CardList
