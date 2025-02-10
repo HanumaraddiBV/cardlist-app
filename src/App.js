46cc890a02d78a6b3770edc2f9b2d8ddb0eec2ef
Hanumaraddi Vaddatti <hanumaraddin14@gmail.com>
2025-02-10 18:57:04 +0530

    added background color

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import CardList from './pages/CardList';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-5">Card List</h1>
+    <div className="bg-gradient-to-tr from-[#C9D6FF] to-[#E2E2E2] flex flex-col items-center  min-h-screen bg-gray-100 top-1">
+      <h1 className="text-3xl font-bold mb-5 mt-2">Cards List</h1>
       <CardList />
     </div>
   );
